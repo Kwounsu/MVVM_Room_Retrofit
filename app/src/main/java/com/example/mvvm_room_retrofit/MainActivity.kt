214c3b2ab43e51b557d498d5c35429c84ed64b37
Kwounsu <sb00lgs@gmail.com>
2020-09-23 09:34:15 -0700

    ViewModel basic structure

--- app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mvvm_room_retrofit
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
@@ -10,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val liveText = MutableLiveData<String>()
+    private val mViewModel: MyViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/mvvm_room_retrofit/ViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm_room_retrofit
+
+import androidx.lifecycle.ViewModel
+
+class MyViewModel: ViewModel() {
+
+}
