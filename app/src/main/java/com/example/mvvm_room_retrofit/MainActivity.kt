b6a95f1580a8e2df4d5a8141661ad32bbab4a9c7
Kwounsu <sb00lgs@gmail.com>
2020-09-23 08:23:04 -0700

    databinding

--- app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.mvvm_room_retrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.mvvm_room_retrofit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var liveText: MutableLiveData<String> = MutableLiveData()
+    private lateinit var binding: ActivityMainBinding
+    val liveText = MutableLiveData<String>()
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        liveText.observe(this, Observer {
-            textView.text = it
-        })
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            lifecycleOwner = this@MainActivity  // **중요** binding에 LifeCycleOwner을 지정해줘야 LiveData가 실시간으로 변화
+            activity = this@MainActivity        // xml 파일에 선언한 activity
 
-        button.setOnClickListener {
-            liveText.value = "Hello World! ${++count}"
+            button.setOnClickListener {
+                liveText.value = "Hello World! ${++count}"
+            }
         }
     }
 }
