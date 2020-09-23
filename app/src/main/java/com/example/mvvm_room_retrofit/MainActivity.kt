1547769dc58419a6ad9018e46daf43f3d9000fc2
Kwounsu <sb00lgs@gmail.com>
2020-09-23 08:12:56 -0700

    livedata - click button counts up and reflect on textview

--- app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mvvm_room_retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var liveText: MutableLiveData<String> = MutableLiveData()
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        liveText.observe(this, Observer {
+            textView.text = it
+        })
+
+        button.setOnClickListener {
+            liveText.value = "Hello World! ${++count}"
+        }
     }
 }
