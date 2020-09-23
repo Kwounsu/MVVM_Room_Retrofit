24f14f4ba8dba8bce33953074b00e667fec741a6
Kwounsu <sb00lgs@gmail.com>
2020-09-23 11:24:08 -0700

    Make DB persistent

--- app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_room_retrofit/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm_room_retrofit.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val liveText = MutableLiveData<String>()
-    private val mViewModel: MyViewModel by viewModels()
+    private val wordViewModel: WordViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +25,24 @@ class MainActivity : AppCompatActivity() {
             lifecycleOwner = this@MainActivity  // **중요** binding에 LifeCycleOwner을 지정해줘야 LiveData가 실시간으로 변화
             activity = this@MainActivity        // xml 파일에 선언한 activity
         }
+
+        // RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
     }
 
     fun updateTextViewWithEditText () {
-        liveText.value = editText.text.toString()
+        editText.text.toString().let { data ->
+            val word = Word(data)
+            wordViewModel.insert(word)
+            Unit
+        }
     }
 }
--- app/src/main/java/com/example/mvvm_room_retrofit/WordRoomDatabase.kt
+++ app/src/main/java/com/example/mvvm_room_retrofit/WordRoomDatabase.kt
@@ -4,14 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-// Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = [Word::class], version = 1, exportSchema = false)
-public abstract class WordRoomDatabase : RoomDatabase() {
+abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
 
@@ -19,61 +15,17 @@ public abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(
-            context: Context,
-            scope: CoroutineScope
-        ): WordRoomDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    WordRoomDatabase::class.java,
-                    "word_database"
-                )
-                    // Wipes and rebuilds instead of migrating if no Migration object.
-                    // Migration is not part of this codelab.
-                    .fallbackToDestructiveMigration()
-                    .addCallback(WordDatabaseCallback(scope))
+                        context.applicationContext,
+                        WordRoomDatabase::class.java,
+                        "word_database"
+                    )
                     .build()
                 INSTANCE = instance
-                // return instance
                 instance
             }
-        }
-
-        private class WordDatabaseCallback(
-            private val scope: CoroutineScope
-        ) : RoomDatabase.Callback() {
-            /**
-             * Override the onOpen method to populate the database.
-             * For this sample, we clear the database every time it is created or opened.
-             */
-            override fun onOpen(db: SupportSQLiteDatabase) {
-                super.onOpen(db)
-                // If you want to keep the data through app restarts,
-                // comment out the following line.
-                INSTANCE?.let { database ->
-                    scope.launch(Dispatchers.IO) {
-                        populateDatabase(database.wordDao())
-                    }
-                }
-            }
-        }
-
-        /**
-         * Populate the database in a new coroutine.
-         * If you want to start with more words, just add them.
-         */
-        fun populateDatabase(wordDao: WordDao) {
-            // Start the app with a clean database every time.
-            // Not needed if you only populate on creation.
-            wordDao.deleteAll()
-
-            var word = Word("Merry")
-            wordDao.insert(word)
-            word = Word("Christmas!")
-            wordDao.insert(word)
         }
     }
 }
